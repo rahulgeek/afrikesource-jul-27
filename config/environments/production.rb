ca3503c1cac6de3c7afef81226c50e8fd44992d3
Rahul <RahulC>
2016-08-11 21:21:51 +0530

    js issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,9 +38,9 @@ Rails.application.configure do
  #   :compress => { :drop_console => true })  if defined? Uglifier
 
   # config.assets.css_compressor = :sass
-
+  config.serve_static_assets = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
