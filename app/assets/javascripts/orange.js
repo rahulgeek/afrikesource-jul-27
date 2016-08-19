a49f2b496c1978786c93b5b6ccb0a82e86749393
Rahul <RahulC>
2016-08-19 18:29:25 +0530

    added condition based formula

--- app/assets/javascripts/orange.js
+++ app/assets/javascripts/orange.js
@@ -1,13 +1,30 @@
 
 $(document).ready(function() {
   $("#usd").on("propertychange change keyup paste input", function(){
-  var rate = $("#currency_rate").text()
   var usd = $("#usd").val()
+  if(parseFloat(usd)<=10)
+  	{$("#valid_usd").text("no less than 10$ can be entered")
+  	$("#proceed").attr("disabled","true")
+  }
+  else
+  {
+  $("#valid_usd").text("")
+  $("#proceed").removeAttr("disabled")
+  var rate = $("#currency_rate").text() 
   var converted_currency = rate*usd
-  var ttus = ((parseFloat(usd)+((usd*0.029)+0.3))*1.035) //c1usd =1
+  if(parseFloat(usd)>10 && parseFloat(usd)<=999)
+  {
+  	var ttus = ((parseFloat(usd)+((usd*0.029)+0.3))*1.05) //c1usd =1
+  }
+  else
+  {
+  	var ttus = ((parseFloat(usd)+((usd*0.029)+0.3))*1.035) //c1usd =1
+  }
+  
   $("#xof").val((converted_currency.toLocaleString()))
   $("#ttus").val(ttus.toLocaleString())
   $("#fees").val((ttus-usd).toLocaleString())
+  }
 });
 });
 
