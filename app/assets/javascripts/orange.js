06afd6f627a9717216d49df3e032f193261f298b
Rahul <RahulC>
2016-08-19 17:13:25 +0530

    formula updated

--- app/assets/javascripts/orange.js
+++ app/assets/javascripts/orange.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
   $("#usd").on("propertychange change keyup paste input", function(){
   var rate = $("#currency_rate").text()
   var usd = $("#usd").val()
-  var ttus = ((((usd *0.029)+0.30*1.05)*(rate+25))/1) //c1usd =1
-  $("#xof").val((rate*usd).toLocaleString())
+  var converted_currency = rate*usd
+  var ttus = ((parseFloat(usd)+((usd*0.029)+0.3))*1.035) //c1usd =1
+  $("#xof").val((converted_currency.toLocaleString()))
   $("#ttus").val(ttus.toLocaleString())
   $("#fees").val((ttus-usd).toLocaleString())
 });
 });
+
+
+
