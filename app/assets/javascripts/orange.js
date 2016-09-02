c5a597205fe4ede2bbc36901139c215f89e4456c
Rahul <RahulC>
2016-09-02 18:28:53 +0530

    fixes for simulation page

--- app/assets/javascripts/orange.js
+++ app/assets/javascripts/orange.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
   	 	$("#xof").val(0)
   		$("#ttus").val(0)
   		$("#fees").val(0)
-      $("#total_cost_usd").text(0)
-      $("#total_cost_fcfa").text(0)
+      // $("#total_cost_usd").text(0)
+      // $("#total_cost_fcfa").text(0)
   }
   else
   {
@@ -32,11 +32,13 @@ $(document).ready(function() {
   $("#xof").val((converted_currency.toLocaleString()))
   $("#ttus").val(ttus.toLocaleString())
   $("#fees").val((ttus-usd).toLocaleString())
-  $("#total_cost_usd").text(ttus)
-  $("#total_cost_fcfa").text(converted_currency.toLocaleString())
+  // $("#total_cost_usd").text(ttus)
+  // $("#total_cost_fcfa").text(converted_currency.toLocaleString())
   }
 });
+
 });
+
 
 
 
