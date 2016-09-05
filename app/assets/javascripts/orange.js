faf2a252bfa65ab82d06debc5f9c176d02c0b754
Rahul <RahulC>
2016-09-05 13:43:58 +0530

    orange page UI
    xof to usd converter

--- app/assets/javascripts/orange.js
+++ app/assets/javascripts/orange.js
@@ -1,14 +1,14 @@
 
 $(document).ready(function() {
-  $("#usd").on("propertychange change keyup paste input", function(){
-  var usd = $("#usd").val()
-  if(parseFloat(usd)<10)
+  $("#xof").on("propertychange change keyup paste input", function(){
+  var xof = $("#xof").val()
+  if(parseFloat(xof)<10)
   	{$("#valid_usd").text("no less than 10$ can be entered")
   	$("#proceed").attr("disabled","true")
   }
-  else if(usd=="")
+  else if(xof=="")
   {
-  	 	$("#xof").val(0)
+  	 	$("#usd").val(0)
   		$("#ttus").val(0)
   		$("#fees").val(0)
       // $("#total_cost_usd").text(0)
@@ -16,10 +16,13 @@ $(document).ready(function() {
   }
   else
   {
+
+  usd = $("#usd").val()
+
   $("#valid_usd").text("")
   $("#proceed").removeAttr("disabled")
   var rate = $("#currency_rate").text() 
-  var converted_currency = rate*usd
+  var converted_currency = rate*xof
   if(parseFloat(usd)>=10 && parseFloat(usd)<=999)
   {
   	var ttus = ((parseFloat(usd)+((usd*0.029)+0.3))*1.05) //c1usd =1
@@ -29,7 +32,7 @@ $(document).ready(function() {
   	var ttus = ((parseFloat(usd)+((usd*0.029)+0.3))*1.035) //c1usd =1
   }
   
-  $("#xof").val((converted_currency.toLocaleString()))
+  $("#usd").val((converted_currency))
   $("#ttus").val(ttus.toLocaleString())
   $("#fees").val((ttus-usd).toLocaleString())
   // $("#total_cost_usd").text(ttus)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def get_fox_rate_for_usd
 		fx = OpenExchangeRates::Rates.new
-		fx.exchange_rate(:from => "USD", :to => "XOF", :on => (Time.zone.now.to_date-1).to_s).round
+		fx.exchange_rate(:from => "XOF", :to => "USD", :on => (Time.zone.now.to_date-1).to_s)
 	end
 end
